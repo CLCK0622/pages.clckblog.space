483f7b066f3d5e17cd2c1a5209b1aa27aaea9f9b
Kevin Zhong <zhongyi070622@163.com>
2022-12-18 09:25:24 +0800

    add comments system

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ const yaml = require("js-yaml");
 const { DateTime } = require("luxon");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const htmlmin = require("html-minifier");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 module.exports = function (eleventyConfig) {
   // Disable automatic use of your .gitignore
@@ -9,6 +10,9 @@ module.exports = function (eleventyConfig) {
 
   // Merge data instead of overriding
   eleventyConfig.setDataDeepMerge(true);
+
+  // Enable the RSS Feed
+  eleventyConfig.addPlugin(pluginRss);
 
   // human readable date
   eleventyConfig.addFilter("readableDate", (dateObj) => {
@@ -60,5 +64,6 @@ module.exports = function (eleventyConfig) {
       input: "src",
     },
     htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk",
   };
 };
